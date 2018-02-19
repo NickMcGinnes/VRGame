3d562ca01c63a302958836d4f72f00965d0afae4
Unknown <c15729315@mydit.ie>
2018-02-19 14:20:12 +0000

    First push

---
+++ VR_Prototype_2018/Assets/Scripts/CLASSNOTES.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CLASSNOTES : MonoBehaviour
+{
+    /*
+     * Think why we are using VR, is it justified?
+     * Experiences, tours, history etc...
+     * Limitations of Google Cardboard, not great for experiences.
+     * Simulation/training
+     * 
+     * Maintain immersion
+     * When to give/take control? Dont take control of camera without the player initiating that action
+     * Can they move?
+     * Player attention
+     * Limited space for movement
+     * Motion sickness, glasses, some people just can't do VR/3D
+     * 
+     * VR/AR forcast to be worth 5.8-29.5bn USD by 2020
+     * Military/Pharmacutical/Simulation/Maintanance/Aviation are big users, games not so much
+     * 
+     */
+}
